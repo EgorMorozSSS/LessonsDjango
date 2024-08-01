886e26435ea2daebf9875946adf76743d08d5416
Egor Moroz <morozil06@mail.ru>
2024-08-01 17:57:22 +0300

    add dictionary and use them on index.html

--- Lesson2/main/views.py
+++ Lesson2/main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html')
+    
+    return render(request, 'main/index.html', {'title': 'Главная страница'})
 
 
 def about(request):
