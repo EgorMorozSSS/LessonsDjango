afe91f8c68c37ec22faa9701d504c3378ce72864
Egor Moroz <morozil06@mail.ru>
2024-08-01 14:46:46 +0300

    add html template

--- Lesson2/main/views.py
+++ Lesson2/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h4>Проверка работы</h4>")
+    return request(request, 'main/index.html')
 
 
 def about(request):
